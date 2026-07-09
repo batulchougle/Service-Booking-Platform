a89757458bb7b0fcf458429c229802a9601a848e
batulchougle <batulchougle@gmail.com>
2026-07-09 12:52:11 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vite.dev/config/
+
+console.log('VITE_API_URL at build time:', process.env.VITE_API_URL)
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
