e5eeb13e9d2a86ba23dbb1b99e5d634c9ca81517
batulchougle <batulchougle@gmail.com>
2026-07-09 16:51:44 +0530

    Update settings.py

--- freelance/settings.py
+++ freelance/settings.py
@@ -174,7 +174,7 @@ CLOUDINARY_STORAGE = {
      'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
      'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
  }
- DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE ='cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 MEDIA_URL = '/media/'
