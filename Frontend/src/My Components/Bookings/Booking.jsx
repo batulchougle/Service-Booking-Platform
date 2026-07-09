790adf5d2c8b2bec909e368c9ea5d8327fe0923f
batulchougle <batulchougle@gmail.com>
2026-07-09 12:13:48 +0530

    Update Booking.jsx

--- Frontend/src/My Components/Bookings/Booking.jsx
+++ Frontend/src/My Components/Bookings/Booking.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Booking.css'
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import axios from 'axios'
+import AxiosInstance from '../../api/axiosInstance'
 import { toast } from 'react-toastify'
 
 
@@ -33,8 +33,8 @@ const Booking = () => {
       }   
       
       try {
-        const response =  await axios.post(
-          `http://localhost:8000/select/payment/method/`, 
+            const response =  await AxiosInstance.post(
+                        `/auth/select/payment/method/`, 
           { payment_method: selectedMethod ,booking_id: booking_id},
          { headers: {
             "Authorization": `Bearer ${token}`, 
